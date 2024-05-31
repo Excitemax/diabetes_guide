2dc0bd77e2b0fd621a7d680f72519c333cd2e423
Excitemax <94905481+Excitemax@users.noreply.github.com>
2024-05-31 12:37:47 +0800

    homepage(content subject to change)

--- index.php
+++ index.php
@@ -2,15 +2,38 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=H">
-    <title>Homepage</title>
+    <meta name="viewport" content="width=device-width, initial-scale=H"></meta>
+    <title>Diabetes Guidelines</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-    <!--Kelompok:
-        1. Ezekiel Griffin Dave/42230011
-        2. 
-        3. 
-    -->
-    
+    <header>
+        <h1>Diabetes Guidelines</h1>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="/auth/register.php">Register</a>
+            <a href="/auth/login.php">Login</a>
+        </nav>
+    </header>
+    <main>
+        <section>
+            <h2>About Diabetes</h2>
+            <!--Isi deskripsi-->
+            <p>
+
+            </p>
+        </section>
+        <section>
+            <h2>Healthy Living Tips</h2>
+            <!--ingat isi-->
+            <ul>
+                <li>isi nanti</li>
+                <li>isi nanti</li>
+                <li>isi nanti.</li>
+                <li>isi nanti</li>
+            </ul>
+        </section>
+    </main>
+    <script src="script.js"></script>
 </body>
 </html>
