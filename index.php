67fde56c8be6518a1b7039aa3f3e51154168d0bc
Excitemax <94905481+Excitemax@users.noreply.github.com>
2024-06-03 15:47:48 +0800

    memperbaiki arah href di index.php

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
         <h1>Diabetes Guidelines</h1>
         <nav>
             <a href="index.php">Home</a>
-            <a href="register.php">Register</a>
-            <a href="login.php">Login</a>
+            <a href="account/register.php">Register</a>
+            <a href="account/login.php">Login</a>
         </nav>
     </header>
     <main>
