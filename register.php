336d3ef136d698f025c8bdfb8e17ef3bcfca54f5
Jerryliong <jeriano21@gmail.com>
2024-06-03 14:20:19 +0800

    website kesehatan uas pemrograman web

---
+++ register.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Register - Website Kesehatan</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="login-container">
+        <h2>Register</h2>
+        <form action="register_process.php" method="post">
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" required>
+            
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" required>
+            
+            <button type="submit">Register</button>
+        </form>
+        <p>Sudah punya akun? <a href="index.php">Login</a></p>
+    </div>
+</body>
+</html>
