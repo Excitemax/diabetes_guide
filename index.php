7aac5006a5fcc32183bc9bb9ce79e98d9d8dde21
Excitemax <94905481+Excitemax@users.noreply.github.com>
2024-06-03 13:08:11 +0800

    Menambah isi konten topik topik di file

--- index.php
+++ index.php
@@ -19,18 +19,44 @@
         <section>
             <h2>About Diabetes</h2>
             <!--Isi deskripsi-->
-            <p>
-
+            <p>Diabetes adalah penyakit kronis yang ditandai dengan tingginya kadar gula darah. 
+                Glukosa merupakan sumber energi utama bagi sel tubuh manusia. 
+                Akan tetapi, pada penderita diabetes, glukosa tersebut tidak dapat digunakan oleh tubuh.
             </p>
+
+            <h2>Jenis Diabetes</h2>
+
+            <p>Diabetes pada umumnya dibagi menjadi dua jenis:</p>
+
+            <h3>Diabetes tipe 1</h3>
+            <p>Diabetes tipe 1 terjadi ketika sistem kekebalan tubuh secara keliru menyerang dan menghancurkan sel-sel pankreas yang memproduksi insulin. 
+                Hal ini menyebabkan kadar glukosa darah meningkat sehingga memicu kerusakan pada organ-organ tubuh.
+                Diabetes tipe 1 dikenal juga dengan diabetes autoimun.
+                Penyebab diabetes tipe 1 masih belum diketahui secara pasti. 
+                Namun, ada dugaan penyakit ini terkait dengan faktor genetik dan faktor lingkungan.
+            </p>
+
+            <h3>Diabetes tipe 2</h3>
+            <p>
+            Diabetes tipe 2 merupakan jenis diabetes yang paling banyak terjadi, yakni sekitar 90–95%. 
+            Diabetes tipe 2 terjadi ketika sel-sel tubuh menjadi kurang sensitif terhadap insulin sehingga insulin yang dihasilkan tidak bisa digunakan dengan baik. 
+            Kondisi ini dikenal juga dengan istilah resistensi insulin.
+            </p>
+
+            
         </section>
         <section>
             <h2>Healthy Living Tips</h2>
             <!--ingat isi-->
             <ul>
-                <li>isi nanti</li>
-                <li>isi nanti</li>
-                <li>isi nanti.</li>
-                <li>isi nanti</li>
+                <li>Mengatur frekuensi dan menu makanan menjadi lebih sehat</li>
+                <li>Rutin berolahraga dan melakukan aktivitas fisik</li>
+                <li>Menjaga berat badan ideal</li>
+                <li>Beristirahat dan tidur yang cukup</li>
+                <li>Berhenti merokok</li>
+                <li>Menghindari konsumsi minuman beralkohol</li>
+                <li>Mengelola stres dengan baik</li>
+                <li>Rutin menjalani pengecekan gula darah, setidaknya sekali dalam 1 tahun</li>
             </ul>
         </section>
     </main>
