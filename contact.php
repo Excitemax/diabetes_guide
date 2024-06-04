21f076c180d7321c0ff33325cc240cd5337bfb16
Excitemax <94905481+Excitemax@users.noreply.github.com>
2024-06-04 15:50:20 +0800

    memperbaiki bug directory ke registe dan login di contact.php dan fact.php

--- contact.php
+++ contact.php
@@ -11,8 +11,8 @@
         <h1>Mengenai Kami<h1>
         <nav>
             <a href="index.php">Home</a>
-            <a href="register.php">Register</a>
-            <a href="login.php">Login</a>
+            <a href="account/register.php">Register</a>
+            <a href="account/login.php">Login</a>
             <a href="contact.php">Contact</a>
             <a href="facts.php">Facts</a>
             <a href="news.php">News</a>
