e1ec012a048b643229d48f078973c7f88fd74f7b
Excitemax <94905481+Excitemax@users.noreply.github.com>
2024-06-04 17:12:10 +0800

    styling register dan login.php

--- account/login.php
+++ account/login.php
@@ -3,7 +3,7 @@
 <html>
 <head>
     <title>Login</title>
-    <link rel="stylesheet" type="text/css" href="../style.css">
+    <link rel="stylesheet" type="text/css" href="../assets/style2.css">
 </head>
 <body>
     <header>
